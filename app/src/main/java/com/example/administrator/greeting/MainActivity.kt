b81a15eaecbcfea3e931cb9e339c15bd27a270c4
Michelle <2eunzizi@gmail.com>
2018-06-12 17:02:01 -0400

    DONE :)

--- app/src/main/java/com/example/administrator/greeting/MainActivity.kt
+++ app/src/main/java/com/example/administrator/greeting/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.administrator.greeting
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        displayInfo()
+    }
+
+    fun displayInfo() {
+        buttonGo.setOnClickListener{
+            var name = editName.text
+            var email = editEmail.text
+
+            textView3.setText("HELLO" + name + "! YOUR EMAIL IS " +  email )
+        }
     }
 }
